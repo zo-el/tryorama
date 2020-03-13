248ac2a90545513afa1e6afc7a0c2523786325a6
freesig <tomrgowan@gmail.com>
2020-03-14 02:07:25 +1100

    threading

--- src/config/spawn.ts
+++ src/config/spawn.ts
@@ -33,7 +33,7 @@ export const spawnLocal: T.SpawnConductorFn = async (player: Player, { handleHoo
     logger.info("Using conductor path: %s", binPath)
     logger.info("Holochain version: %s", version)
     if(name == '0') {
-    handle = spawn('vtune' , ['-collect', 'threading', '-r', '/home/freesig/fast/' + player.name, '--', binPath, '-c', configPath], {
+    handle = spawn('vtune' , ['-collect', 'hotspots', '-r', '/home/freesig/fast/' + player.name, '--', binPath, '-c', configPath], {
       env: {
         "N3H_QUIET": "1",
         "RUST_BACKTRACE": "1",
